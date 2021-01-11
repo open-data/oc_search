1e186dde58500c8b9d659455d82be34d388cd73d
thriuin <ross.thompson@tbs-sct.gc.ca>
2021-01-11 15:54:32 -0500

    Add base path to python path in uwsgi.py

--- oc_search/wsgi.py
+++ oc_search/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_search.settings')
-
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 application = get_wsgi_application()
