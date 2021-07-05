72f73bdfefd30b29c71308a3ed2e4ec90bfc023e
thriuin <ross.thompson@tbs-sct.gc.ca>
2021-07-05 14:08:30 -0400

    Tweak the UUID RE to be a little less restrictive.

--- ramp/views.py
+++ ramp/views.py
@@ -19,7 +19,7 @@ class RampView(View):
                     '[a-f0-9]{8}-' +
                     '[a-f0-9]{4}-' +
                     version + '[a-f0-9]{3}-' +
-                    '[89ab][a-f0-9]{3}-' +
+                    '[a-f0-9]{4}-' +
                     '[a-f0-9]{12}$'
             ),
             re.IGNORECASE
